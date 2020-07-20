79aeae7edd59230417cd8fcbb9d364452fc5f95f
Joel Croitoru <jcroitoru@gmail.com>
2020-07-20 16:54:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length==2
     return array.join(" and ")
   elsif array.length>=3
-    array.last.insert(-1, "and")
+    array.last
+    insert(-1, "and")
     array.join(", ")
 
   end
