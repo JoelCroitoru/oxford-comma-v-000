0270e6f3be9955554c6147817ef20bac1a09153c
Joel Croitoru <jcroitoru@gmail.com>
2020-07-20 16:38:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
   elsif array.length==2
     return array.join(" and ")
   elsif array.length>=3
-    return array.join(", ")
+    array.join(", ")
+    array.last
+    array.insert(array.last, "and")
+    
   end
 end
