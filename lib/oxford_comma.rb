df71d5b5fae5f9f52ecd4dddcb62a39d3e537ec9
Joel Croitoru <jcroitoru@gmail.com>
2020-07-20 16:48:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length>=3
     array.last
-    last.insert(-2, "and")
+    last.insert(-1, "and")
     array.join(", ")
 
   end
